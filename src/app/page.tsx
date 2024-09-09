3f5b692deac4bc6670922c9727cdc3334e179b27
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-09-09 22:29:29 +0530

    Fixed types & const issues
    Signed-off-by: Chandan-6 <khamitkarsaichandan1035@gmail.com>
    
    * few variables are unused, not re-assigned.
    * So commented the unused.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
           Black Shadow
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          Protecting the Twitter, Authenticity. It's All That Matters.
+          Protecting the Twitter, Authenticity. It&apos;s All That Matters.
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
           <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            Let's Go
+            Let&apos;s Go
           </button>
         </div>
       </Vortex>
